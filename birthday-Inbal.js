f6c1960f8571d4c3ca4c9de498640fcbb9ee9e52
Ophir Eden <ophireden@gmail.com>
2020-10-29 11:37:39 +0200

    1st version

---
+++ birthday-Inbal.js
@@ -0,0 +1,16 @@
+let button = document.getElementById('button');
+button.onclick = ()=> {
+    let output = document.getElementById('output');
+    let showGreeting;
+    let randomGreeting = Math.floor(Math.random()*3);
+    switch(randomGreeting){
+        case 0:showGreeting = 'happy birthday'
+        break;
+        case 1: showGreeting = 'a great year'
+        break;
+        case 2:
+        case 3: showGreeting = 'you are a great teacher'
+        break;
+    }
+    output.innerHTML = showGreeting;
+}
