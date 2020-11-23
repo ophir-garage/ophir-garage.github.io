ec4f1329eef26749fb6d85a00c8be288d6c4b58c
Ophir Eden <ophireden@gmail.com>
2020-11-23 18:34:27 +0200

    added graph

--- source.js
+++ source.js
@@ -18,6 +18,38 @@ button.onclick = () => {
     output.innerHTML = 'your cps is : ' + cps.toFixed(2);
     let bestOutput = document.getElementById('reord');
     bestOutput.innerHTML = 'your best is : ' + bestCPS.toFixed(2);
-    old_time = new_time
+    old_time = new_time;
+    addData(chart,'0',cps);
+
 
 };
+
+// graph plot
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: [],
+        datasets: [{
+            lineTension: 0,
+            //label: 'My First dataset',
+            //backgroundColor: 'rgb(255, 99, 132)',
+            //borderColor: 'rgb(255, 99, 132)',
+            data: []
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
